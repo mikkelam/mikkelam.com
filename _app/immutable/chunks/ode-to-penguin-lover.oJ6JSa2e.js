50c68351f2e8e8741676386f11631ea014527146
mikkelam <mikkelarm@gmail.com>
2024-07-20 19:07:27 +0200

    Updates

---
+++ _app/immutable/chunks/ode-to-penguin-lover.oJ6JSa2e.js
@@ -0,0 +1,15 @@
+import{s as c,a as l,e as u,n as h}from"./scheduler.Bnrw85MU.js";import{S as _,i as f,c as p,a as g,m as v,t as y,b as x,d as b,e as $,f as w,g as k,h as C,j as O,k as P}from"./index.B98wcXj2.js";import{P as S,g as W,a as d}from"./PostLayout.22MeB5ub.js";function Y(s){let e,n=`Oh penguin lover, you are among the finest around.
+In the world of code, your skills truly astound.
+With open-source fervor, you craft with delight,
+Lines of pure magic, in the depths of the night.
+
+Commanding the terminal with grace and ease.
+You navigate systems like a brisk, gentle breeze.
+From kernel to shell, you master each part.
+Your passion for 🐧 is born from the heart.
+
+With every keystroke, you weave digital art.
+In the vast cyber ocean, you’re a guiding chart.
+Without you, the world would surely fall,
+Yet all you get is a +1 on GitHub. That’s all.
+`;return{c(){e=$("pre"),e.textContent=n,this.h()},l(a){e=w(a,"PRE",{class:!0,"data-svelte-h":!0}),k(e)!=="svelte-ve0d7q"&&(e.textContent=n),this.h()},h(){C(e,"class","highlighted-poem max-w-[540px] ")},m(a,r){O(a,e,r)},p:h,d(a){a&&P(e)}}}function j(s){let e,n;const a=[s[0],m];let r={$$slots:{default:[Y]},$$scope:{ctx:s}};for(let t=0;t<a.length;t+=1)r=l(r,a[t]);return e=new S({props:r}),{c(){p(e.$$.fragment)},l(t){g(e.$$.fragment,t)},m(t,o){v(e,t,o),n=!0},p(t,[o]){const i=o&1?W(a,[o&1&&d(t[0]),o&0&&d(m)]):{};o&2&&(i.$$scope={dirty:o,ctx:t}),e.$set(i)},i(t){n||(y(e.$$.fragment,t),n=!0)},o(t){x(e.$$.fragment,t),n=!1},d(t){b(e,t)}}}const m={title:"Ode to a penguin lover🐧",date:"2024-07-20T00:00:00.000Z",published:!0,outline:"Poem about OSS devs",layout:"post"};function q(s,e,n){return s.$$set=a=>{n(0,e=l(l({},e),u(a)))},e=u(e),[e]}class z extends _{constructor(e){super(),f(this,e,q,j,c,{})}}export{z as default,m as metadata};
